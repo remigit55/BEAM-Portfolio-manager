23100a5518ef07600c024b7049d66640d695d066
remigit55 <125653129+remigit55@users.noreply.github.com>
2025-06-18 19:45:39 +0200

    Update portefeuille.py

--- portefeuille.py
+++ portefeuille.py
@@ -64,6 +64,7 @@ def afficher_portefeuille():
             text-align: right;
             border-bottom: 1px solid transparent;
             border-left: 1px solid transparent;
+            border-right: 1px solid transparent;
             font-family: "Aptos narrow", Helvetica;
             font-size: 11px;
         }
