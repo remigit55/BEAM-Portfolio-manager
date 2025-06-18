339b9755c5072b8668e82d20d11ba2927ed2a9ac
remigit55 <125653129+remigit55@users.noreply.github.com>
2025-06-19 00:31:31 +0200

    Update od_comptables.py

--- od_comptables.py
+++ od_comptables.py
@@ -7,13 +7,4 @@ import time
 
 def afficher_od_comptables():
 
-    if "fx_rates" in st.session_state and st.session_state.fx_rates:
-        devise_cible = st.session_state.get("devise_cible", "EUR")
-        st.markdown(f"Taux appliqués pour conversion en **{devise_cible}** au **{datetime.date.today()}**")
-        fx_df = pd.DataFrame(
-            list(st.session_state.fx_rates.items()),
-            columns=["Conversion", "Taux"]
-        )
-        st.dataframe(fx_df, use_container_width=True)
-    else:
-        st.info("Aucun taux de change disponible. Veuillez d'abord importer un portefeuille.")
+
