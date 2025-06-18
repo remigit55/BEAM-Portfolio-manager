92c373c8463da7f2c380489a8ee38616734eb9c1
remigit55 <125653129+remigit55@users.noreply.github.com>
2025-06-19 01:53:08 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -55,7 +55,7 @@ except Exception:
 st.markdown(
     f"""
     <div style="display: flex; align-items: left; margin-top: -10px; margin-bottom: 20px;">
-        <img src="data:image/png;base64,{logo_base64}" style="height: 50px; margin-right: 12px;" />
+        <img src="data:image/png;base64,{logo_base64}" style="height: 70px; margin-right: 5px;" />
         <h1 style='font-size: 32px; margin: 0;'>BEAM Portfolio Manager</h1>
     </div>
     """,
