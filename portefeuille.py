ef14852c6bfe69e308113114100c9aac31df7a6d
remigit55 <125653129+remigit55@users.noreply.github.com>
2025-06-18 18:54:04 +0200

    Update portefeuille.py

--- portefeuille.py
+++ portefeuille.py
@@ -94,7 +94,7 @@ def afficher_portefeuille():
 
     html += f"""
         <tr class="total-row">
-            <td colspan="1" style="text-align:right;">TOTAL</td>
+            <td colspan="1" style="text-align:left;">TOTAL</td>
             <td></td>
             <td></td>
             <td></td>
