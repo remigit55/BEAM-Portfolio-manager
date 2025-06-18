7a5777fc804393649c6cb767d0b3f9888c8a3d48
remigit55 <125653129+remigit55@users.noreply.github.com>
2025-06-18 23:18:16 +0200

    Update portefeuille.py

--- portefeuille.py
+++ portefeuille.py
@@ -282,7 +282,7 @@ function attachListeners() {
 
 // Attachement initial
 attachListeners();
-setTimeout(attachListeners, 1000);
+#setTimeout(attachListeners, 1000);
 
 // MutationObserver pour les re-rendus
 var observer = new MutationObserver(() => {
