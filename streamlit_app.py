7fc02649c1064559155a8f622f34ff837dc5e352
remigit55 <125653129+remigit55@users.noreply.github.com>
2025-06-19 01:59:42 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -40,20 +40,13 @@ st.markdown(f"""
 
         /* Personnalisation des onglets */
         div[data-testid="stTabs"] button[role="tab"] {{
-            background-color: #f5f5f5 !important;
+            background-color: ACCENT_COLOR !important;
             color: #888 !important;
             border: none;
             padding: 10px 15px;
             font-size: 14px;
             font-weight: normal;
             border-radius: 5px 5px 0 0 !important;
-        }}
-
-        div[data-testid="stTabs"] button[role="tab"][aria-selected="true"] {{
-            background-color: {SECONDARY_COLOR} !important;
-            color: {PRIMARY_COLOR} !important;
-            font-weight: bold !important;
-            border-bottom: 3px solid {ACCENT_COLOR} !important;
         }}
     </style>
 """, unsafe_allow_html=True)
