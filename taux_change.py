11088e5922aa5b79320ac0b5a59a5c7e542a0cd7
remigit55 <125653129+remigit55@users.noreply.github.com>
2025-06-19 01:22:37 +0200

    Update taux_change.py

--- taux_change.py
+++ taux_change.py
@@ -82,3 +82,5 @@ def afficher_taux_change():
 </div>
 """
     components.html(html_code, height=400, scrolling=True)
+
+st.markdown(f"_Dernière mise à jour : {datetime.datetime.now().strftime('%H:%M:%S')}_")
