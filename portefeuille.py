fd88ef5fd6f59e89daeec7019cce2079a160e9b6
remigit55 <125653129+remigit55@users.noreply.github.com>
2025-06-19 16:23:07 +0200

    Update portefeuille.py

--- portefeuille.py
+++ portefeuille.py
@@ -25,15 +25,15 @@ def afficher_portefeuille():
         
     # Recalcul des taux ici si nécessaire
         def fetch_fx_rates(base="EUR"):
-        try:
-            url = f"https://api.exchangerate.host/latest?base={base}"
-            response = requests.get(url, timeout=10)
-            response.raise_for_status()
-            data = response.json()
-            return data.get("rates", {})
-        except Exception as e:
-            print(f"Erreur lors de la récupération des taux : {e}")
-            return {}
+            try:
+                url = f"https://api.exchangerate.host/latest?base={base}"
+                response = requests.get(url, timeout=10)
+                response.raise_for_status()
+                data = response.json()
+                return data.get("rates", {})
+            except Exception as e:
+                print(f"Erreur lors de la récupération des taux : {e}")
+                return {}
         
         st.session_state.fx_rates = fetch_fx_rates()
     fx_rates = st.session_state.get("fx_rates", {})
