edc076d643dcefa59d3a97d636942a62176704c2
remigit55 <125653129+remigit55@users.noreply.github.com>
2025-06-19 15:58:49 +0200

    Update taux_change.py

--- taux_change.py
+++ taux_change.py
@@ -1,7 +1,7 @@
 import streamlit as st
 import pandas as pd
 import datetime
-import requests
+import requests  # Ensure requests is imported
 import html
 import streamlit.components.v1 as components
 
