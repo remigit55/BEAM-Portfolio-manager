7bef97e247d3d452fdfd9f0020a771ba60358ea5
remigit55 <125653129+remigit55@users.noreply.github.com>
2025-06-20 13:57:44 +0200

    Delete test_download.py

--- test_download.py
+++
@@ -1,9 +0,0 @@
-import yfinance as yf
-from datetime import datetime, timedelta
-
-ticker = "GLDG"  # ou un autre ticker que tu utilises
-start = datetime.today() - timedelta(days=365)
-end = datetime.today()
-
-df = yf.download(ticker, start=start, end=end, interval="1d")
-print(df.tail())
