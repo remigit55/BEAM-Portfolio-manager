39e59152d52857d7c9f4a92ffb28cfc73b4b5121
remigit55 <125653129+remigit55@users.noreply.github.com>
2025-06-23 15:07:02 +0200

    Update portfolio_display.py

--- portfolio_display.py
+++ portfolio_display.py
@@ -461,7 +461,7 @@ def afficher_synthese_globale(total_valeur, total_actuelle, total_h52, total_lt)
 
 
     # --- Nouveau Tableau de Répartition par Catégorie ---
-    st.markdown(#### "Répartition et Objectifs par Catégorie")
+    st.markdown("#### Répartition et Objectifs par Catégorie")
 
     # Définition des allocations cibles par catégorie
     target_allocations = {
