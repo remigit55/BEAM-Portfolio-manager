987310402d976a0712e9a8ed4ce6ddae818b32dd
remigit55 <125653129+remigit55@users.noreply.github.com>
2025-06-26 21:53:55 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -254,3 +254,9 @@ def main():
 
 if __name__ == "__main__":
     main()
+
+if st.button("🔄 Réinitialiser la session"):
+    for key in list(st.session_state.keys()):
+        del st.session_state[key]
+    st.experimental_rerun()
+
