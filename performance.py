853ee3f56e86207ed95f47948c178dcdd4ffdba1
remigit55 <125653129+remigit55@users.noreply.github.com>
2025-06-28 23:25:21 +0200

    Update performance.py

--- performance.py
+++ performance.py
@@ -36,21 +36,19 @@ def display_performance_history():
     # --- SÉLECTION DE PÉRIODE AVEC ST.RADIO (COMPOSANT NATIF STREAMLIT) ---
 
     period_options = {
-        "1 Semaine": timedelta(weeks=1),
-        "1 Mois": timedelta(days=30),
-        "3 Mois": timedelta(days=90),
-        "6 Mois": timedelta(days=180),
-        "1 An": timedelta(days=365),
-        "5 Ans": timedelta(days=365 * 5),
-        "10 Ans": timedelta(days=365 * 10),
+        "1W": timedelta(weeks=1),
+        "1M": timedelta(days=30),
+        "3M": timedelta(days=90),
+        "6M": timedelta(days=180),
+        "1Y": timedelta(days=365),
+        "5Y": timedelta(days=365 * 5),
+        "10Y": timedelta(days=365 * 10),
     }
     
     # Options pour le sélecteur, avec la période par défaut
     period_labels = list(period_options.keys())
     default_period_index = period_labels.index(st.session_state.get("selected_ticker_table_period_label", "1 Semaine"))
 
-
-    st.markdown("#### Sélection de la période d'affichage des cours")
     selected_label = st.radio(
         "Choisissez une période:",
         period_labels,
@@ -67,8 +65,6 @@ def display_performance_history():
 
     end_date_table = datetime.now().date()
     start_date_table = end_date_table - selected_period_td
-
-    st.info(f"Affichage des cours de clôture pour les tickers du portefeuille sur la période : {start_date_table.strftime('%d/%m/%Y')} à {end_date_table.strftime('%d/%m/%Y')}.")
 
     with st.spinner("Récupération des cours des tickers en cours..."):
         last_days_data = {}
