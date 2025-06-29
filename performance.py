e71c0ac6adfdf7fbddab3591dbccbab2ee9d9f1b
remigit55 <125653129+remigit55@users.noreply.github.com>
2025-06-30 00:00:30 +0200

    Update performance.py

--- performance.py
+++ performance.py
@@ -83,7 +83,6 @@ def display_performance_history():
                 for date_idx, price in filtered_data.items():
                     fx_key = ticker_devise
                     fx_rate_for_date = fx_rates.get(fx_key, 1.0)
-                    # Conversion penny -> pound si besoin
                     if ticker_devise.upper() == "GBp":
                         price = price / 100
                         ticker_devise = "GBP"
