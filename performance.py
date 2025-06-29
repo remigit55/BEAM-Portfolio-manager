02ffd8d4c04f6816db6e24dd177587b5e7ba5ff1
remigit55 <125653129+remigit55@users.noreply.github.com>
2025-06-29 22:49:15 +0200

    Update performance.py

--- performance.py
+++ performance.py
@@ -68,7 +68,7 @@ def display_performance_history():
         "1Y": timedelta(days=365), 
         "5Y": timedelta(days=365 * 5),
         "10Y": timedelta(days=365 * 10),
-        "Max": timedelta(days=365 * 50)
+        "20Y": timedelta(days=365 * 20)
     }
     period_labels = list(period_options.keys())
     
